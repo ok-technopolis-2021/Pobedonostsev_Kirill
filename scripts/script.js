0eb35ab770a5da80c1e329de31244275eeeed4b4
Kirill Pobedonostsev <pobedonoscevkirill@gmail.com>
2021-11-18 16:43:05 +0300

    Correct the theme change script

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 const switcher = document.getElementById('switcher');
 
-switcher.addEventListener('change', (e) => {
-    if (e.target.checked) {
-        document.body.classList.toggle('dark-theme')
-        document.body.classList.toggle('light-theme')
+switcher.addEventListener('change', (event) => {
+    if (event.target.checked) {
+        document.body.classList.add('dark-theme');
+        document.body.classList.remove('light-theme');
     } else {
-        document.body.classList.toggle('dark-theme')
-        document.body.classList.toggle('light-theme')
+        document.body.classList.remove('dark-theme');
+        document.body.classList.add('light-theme');
     }
 });
