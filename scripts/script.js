9d2a3452fbb6d7d7f4d338d8d81ac968bf450543
Kirill Pobedonostsev <pobedonoscevkirill@gmail.com>
2021-11-18 15:36:58 +0300

    Make the theme change more correct

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 document.body.addEventListener('click', event => {
     if (event.target === document.body) {
-        document.body.classList.toggle('body_dark')
+        document.body.classList.toggle('dark-theme')
     }
 });
