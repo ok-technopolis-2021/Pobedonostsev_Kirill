2128a5514dc41fc380e2ff846da322b673310888
Kirill Pobedonostsev <pobedonoscevkirill@gmail.com>
2021-11-18 16:02:28 +0300

    Add theme switcher

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,11 @@
-document.body.addEventListener('click', event => {
-    if (event.target === document.body) {
+const switcher = document.getElementById('switcher');
+
+switcher.addEventListener('change', (e) => {
+    if (e.target.checked) {
         document.body.classList.toggle('dark-theme')
+        document.body.classList.toggle('light-theme')
+    } else {
+        document.body.classList.toggle('dark-theme')
+        document.body.classList.toggle('light-theme')
     }
 });
